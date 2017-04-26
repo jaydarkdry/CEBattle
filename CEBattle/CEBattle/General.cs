c75a193761f8edae25c15751858e3583dd12b37a
euphoricnirvana <hetucam@gmail.com>
2017-04-25 20:37:16 -0400

    cebattle: Populate General constructor and Validate

--- CEBattle/CEBattle/General.cs
+++ CEBattle/CEBattle/General.cs
@@ -19,21 +19,47 @@ namespace CEBattle
         public float[] Armies { get; set; }
 
         // Empty constructor, need to be populate later
-        public General()
+        public General(string name, int nbarmy, bool saboteur, Config.Attitude att, float[] armies )
         {
+            Name = name;
+            NbArmy = nbarmy;
+            Saboteur = saboteur;
+            Att = att;
+            Armies = armies;
+
             Armies = new float[Config.UnitType];
             
             for(int i=0; i<Armies.Length; i++)
             {
-
+                Armies[i] = armies[i];
             }
         }
-        
+
+        public General()
+        {
+            Saboteur = false;
+            Att = Config.Attitude.Neutral;
+        }
+
 
         public Boolean Validate()
         {
-            //TODO
+            if (Name == null || Name == "")
+            {
+                return false;
 
+            }
+            if (NbArmy == 0)
+            {
+                return false;
+
+            }
+            if (Armies.Length == 0)
+            {
+                return false;
+
+            }
+            
             return true;
         }
     }
